c38cc7c36c1c5f5d2e8abaced5f1104540ef45f1
huangshanming <1581400575@qq.com>
2023-08-01 14:23:56 +0800

    链路追踪入口文件

--- module.go
+++ module.go
@@ -1,49 +1,25 @@
-package fSchedule
+package linkTrack
 
 import (
-	"fmt"
-	"github.com/farseer-go/fs"
-	"github.com/farseer-go/fs/configure"
-	"github.com/farseer-go/fs/modules"
-	"github.com/farseer-go/fs/timingWheel"
-	"github.com/farseer-go/webapi"
+	"farseer-go/fs/configure"
+	"farseer-go/fs/flog"
 )
 
 type Module struct {
 }
 
-func (module Module) DependsModule() []modules.FarseerModule {
-	return []modules.FarseerModule{webapi.Module{}}
+func (module Module) Initialize() {
+	linkTrackConfigs := configure.GetSubNodes("LinkTrack")
+	for _, configString := range linkTrackConfigs {
+		config := configure.ParseString[linkTrackConfig](configString.(string))
+		if config.TimeInterval == 0 {
+			_ = flog.Error("LinkTrack配置缺少TimeInterval")
+			continue
+		}
+	}
 }
 
 func (module Module) PreInitialize() {
-	// 服务端配置
-	defaultServer = serverVO{
-		Address: configure.GetSlice("FSchedule.Server.Address"),
-		Token:   configure.GetString("FSchedule.Server.Token"),
-	}
-
-	// 客户端配置
-	NewClient()
-	timingWheel.Start()
-}
-
-func (module Module) PostInitialize() {
-	webapi.Area("/api/", func() {
-		webapi.RegisterPOST("/check", Check)
-		webapi.RegisterPOST("/invoke", Invoke)
-		webapi.RegisterPOST("/status", Status)
-		webapi.RegisterPOST("/kill", Kill)
-	})
-	webapi.UseApiResponse()
-	webapi.UsePprof()
-	go webapi.Run(fmt.Sprintf("%s:%d", defaultClient.ClientIp, defaultClient.ClientPort))
-
-	fs.AddInitCallback("注册客户端", func() {
-		defaultClient.RegistryClient()
-	})
-}
-
-func (module Module) Shutdown() {
-	defaultClient.LogoutClient()
+	linkTrack := linkTrack{}
+	linkTrack.Init()
 }
