441b841c3ad8721e2a2cd7564fbc3669202a25c9
steden <1470804@qq.com>
2023-10-19 00:23:05 +0800

    依赖链路追踪组件

--- traceDetailHttp.go
+++ traceDetailHttp.go
@@ -8,8 +8,8 @@ import (
 
 type TraceDetailHttp struct {
 	TraceDetail
-	Key   string // redis key
-	Field string // hash field
+	Method string // post/get/put/delete
+	Url    string // url
 }
 
 func (receiver *TraceDetailHttp) GetTraceDetail() *TraceDetail {
@@ -17,15 +17,15 @@ func (receiver *TraceDetailHttp) GetTraceDetail() *TraceDetail {
 }
 
 func (receiver *TraceDetailHttp) ToString() string {
-	return fmt.Sprintf("[%s]耗时：%s，%s Key=%s，FieldKey=%s", flog.Yellow(receiver.CallType.ToString()), flog.Red(receiver.UseTs.String()), receiver.CallMethod, receiver.Key, receiver.Field)
+	return fmt.Sprintf("[%s]耗时：%s，%s [%s]%s", flog.Yellow(receiver.CallType.ToString()), flog.Red(receiver.UseTs.String()), receiver.CallMethod, receiver.Method, receiver.Url)
 }
 
 // TraceHttp http埋点
-func TraceHttp(method string, key string, field string) *TraceDetailHttp {
+func TraceHttp(method string, url string) *TraceDetailHttp {
 	detail := &TraceDetailHttp{
 		TraceDetail: newTraceDetail(eumCallType.Http, method),
-		Key:         key,
-		Field:       field,
+		Method:      method,
+		Url:         url,
 	}
 	add(detail)
 	return detail
