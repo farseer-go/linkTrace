776f0518634f7a1aef097fc7afb3e563364b41fb
steden <1470804@qq.com>
2023-10-26 22:56:55 +0800

    调整日志格式

--- trackContext.go
+++ trackContext.go
@@ -119,14 +119,16 @@ func (receiver *TraceContext) printLog() {
 				lst.Add(fmt.Sprintf("%s:%s %s 出错了：%s", detail.Exception.CallFile, flog.Blue(detail.Exception.CallLine), flog.Red(detail.Exception.CallFuncName), flog.Red(detail.Exception.ExceptionMessage)))
 			}
 		}
+
+		if receiver.Exception.IsException {
+			lst.Add(fmt.Sprintf("%s%s:%s %s %s", flog.Red("【异常】"), flog.Blue(receiver.Exception.CallFile), flog.Blue(receiver.Exception.CallLine), flog.Green(receiver.Exception.CallFuncName), flog.Red(receiver.Exception.ExceptionMessage)))
+		}
+
 		switch receiver.TraceType {
 		case eumTraceType.WebApi:
-			flog.Printf("【%s链路追踪】TraceId:%s，耗时：%s，%s：\n%s\n", receiver.TraceType.ToString(), flog.Green(parse.ToString(receiver.TraceId)), flog.Red(receiver.UseTs.String()), receiver.Web.Path, strings.Join(lst.ToArray(), "\n"))
+			flog.Printf("【%s链路追踪】TraceId:%s，耗时：%s，%s\n%s\n", receiver.TraceType.ToString(), flog.Green(parse.ToString(receiver.TraceId)), flog.Red(receiver.UseTs.String()), receiver.Web.Path, strings.Join(lst.ToArray(), "\n"))
 		default:
-			flog.Printf("【%s链路追踪】TraceId:%s，耗时：%s：\n%s\n", receiver.TraceType.ToString(), flog.Green(parse.ToString(receiver.TraceId)), flog.Red(receiver.UseTs.String()), strings.Join(lst.ToArray(), "\n"))
-		}
-		if receiver.Exception.IsException {
-			_ = flog.Errorf(fmt.Sprintf("%s:%s %s %s", receiver.Exception.CallFile, flog.Blue(receiver.Exception.CallLine), flog.Green(receiver.Exception.CallFuncName), flog.Red(receiver.Exception.ExceptionMessage)))
+			flog.Printf("【%s链路追踪】TraceId:%s，耗时：%s\n%s\n", receiver.TraceType.ToString(), flog.Green(parse.ToString(receiver.TraceId)), flog.Red(receiver.UseTs.String()), strings.Join(lst.ToArray(), "\n"))
 		}
 		fmt.Println("-----------------------------------------------------------------")
 	}
