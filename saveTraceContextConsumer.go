e78373f8a0cebe4ed23c7e2af7f1ef485b3d64e5
steden <1470804@qq.com>
2024-01-08 15:33:28 +0800

    升级测试覆盖率

--- saveTraceContextConsumer.go
+++ saveTraceContextConsumer.go
@@ -1,10 +1,17 @@
 package linkTrace
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
 	"github.com/farseer-go/collections"
+	"github.com/farseer-go/fs/configure"
+	"github.com/farseer-go/fs/container"
+	"github.com/farseer-go/fs/core"
 	"github.com/farseer-go/fs/exception"
-	"github.com/farseer-go/fs/fops"
+	"github.com/farseer-go/fs/parse"
 	"github.com/farseer-go/fs/trace"
+	"net/http"
 	"time"
 )
 
@@ -20,11 +27,40 @@ func SaveTraceContextConsumer(subscribeName string, lstMessage collections.ListA
 		dto := (*item).(TraceContext)
 		lstTraceContext.Add(dto)
 	})
-	if err := fops.UploadTrace(lstTraceContext); err != nil {
+	if err := uploadTrace(lstTraceContext); err != nil {
 		exception.ThrowRefuseException(err.Error())
 	}
 
 	// 控制3秒执行一次
 	<-time.After(3 * time.Second)
 	return
+}
+
+type UploadTraceRequest struct {
+	List any
+}
+
+// UploadTrace 上传链路记录
+func uploadTrace(lstTraceContext any) error {
+	bodyByte, _ := json.Marshal(UploadTraceRequest{List: lstTraceContext})
+	url := configure.GetFopsServer() + "linkTrace/upload"
+	newRequest, _ := http.NewRequest("POST", url, bytes.NewReader(bodyByte))
+	newRequest.Header.Set("Content-Type", "application/json")
+	// 链路追踪
+	if traceContext := container.Resolve[trace.IManager]().GetCurTrace(); traceContext != nil {
+		newRequest.Header.Set("Trace-Id", parse.ToString(traceContext.GetTraceId()))
+		newRequest.Header.Set("Trace-App-Name", core.AppName)
+	}
+	client := &http.Client{}
+	rsp, err := client.Do(newRequest)
+	if err != nil {
+		return fmt.Errorf("上传链路记录到FOPS失败：%s", err.Error())
+	}
+
+	apiRsp := core.NewApiResponseByReader[any](rsp.Body)
+	if apiRsp.StatusCode != 200 {
+		return fmt.Errorf("上传链路记录到FOPS失败（%v）：%s", rsp.StatusCode, apiRsp.StatusMessage)
+	}
+
+	return err
 }
